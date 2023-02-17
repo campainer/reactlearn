15bc2ec78c521c26920de58862c286ad4dce9184
Verma <ar-anand.verma@in-c02dq37pml7l.intra.rakuten.co.jp>
2023-02-17 10:29:33 +0530

    cleared everything

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Component />
+      <Component />
+      <Component />
+      <Component />
+    </>
   );
 }
+
+const Component = () => {
+  return <div>Component</div>;
+};
 
 export default App;
